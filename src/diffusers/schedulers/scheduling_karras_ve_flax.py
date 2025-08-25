48249e40f01fb8837dbea0294d98b057915699a4
RKUZNETSOV <rkuznetsov@dynamicsun.ru>
2025-08-26 00:05:22 +0300

    remove : random.KeyArray

--- src/diffusers/schedulers/scheduling_karras_ve_flax.py
+++ src/diffusers/schedulers/scheduling_karras_ve_flax.py
@@ -139,7 +139,7 @@ class FlaxKarrasVeScheduler(FlaxSchedulerMixin, ConfigMixin):
         state: KarrasVeSchedulerState,
         sample: jnp.ndarray,
         sigma: float,
-        key: random.KeyArray,
+        key,
     ) -> Tuple[jnp.ndarray, float]:
         """
         Explicit Langevin-like "churn" step of adding noise to the sample according to a factor gamma_i ≥ 0 to reach a
